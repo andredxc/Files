f88604f902848e88ea9444376d54ec992bd2f750
André Dexheimer <andre.dxc@hotmail.com>
2019-03-21 14:53:51 -0300

    Fixing import issues with bouncing balls

--- boucingBalls/Ball.js
+++ boucingBalls/Ball.js
@@ -2,28 +2,42 @@
 var canvas = document.querySelector('canvas');
 var ctx = canvas.getContext('2d');
 
-function Ball(xPos, yPos, xVel, yVel, radius, color){
+var canvasWidth = canvas.width = window.innerWidth;
+var canvasHeight = canvas.height = window.innerHeight;
 
-    // TODO: Check for valid positions and radius
-    this.xPos = xPos;
-    this.yPos = yPos;
-    this.xVel = xVel;
-    this.yVel = yVel;
-    this.color = color;
-    this.radius = radius;
+export default class Ball{
+
+    constructor(xPos, yPos, xVel, yVel, radius, color){
+        // TODO: Check for valid positions and radius
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.xVel = xVel;
+        this.yVel = yVel;
+        this.color = color;
+        this.radius = radius;
+    }
+
+    update(){
+
+        if((this.xPos + this.radius) >= canvasWidth){
+            this.xVel = -this.xVel;
+        }
+
+        if((this.yPos + this.radius) >= canvasHeight){
+            this.yVel1 = -this.yVel;
+        }
+
+        this.xPos += this.xVel;
+        this.yPos += this.yVel;
+
+        console.log(`Position: (${this.xPos}, ${this.yPos}) + (${this.xVel}, ${this.yVel})`);
+    }
+
+    draw(){
+        /* Draw the shape on screen */
+        ctx.beginPath();
+        ctx.fillStype = this.color;
+        ctx.arc(this.xPos, this.yPos, this.radius, 0, 2*Math.PI);
+        ctx.fill();
+    }
 }
-
-Ball.prototype.update = function(){
-    this.xPos += this.xVel;
-    this.yPos += this.yVel;
-};
-
-Ball.prototype.draw = function(){
-    /* Draw the shape on screen */
-    ctx.beginPath();
-    ctx.fillStype = this.color;
-    ctx.arc(this.xPos, this.yPos, this.radius, 0, 2*Math.PI);
-    ctx.fill();
-};
-
-module.exports = Ball;
--- boucingBalls/main.js
+++ boucingBalls/main.js
@@ -1,19 +1,21 @@
-
-/* 
+import Ball from './Ball.js';
+/*
     Andre Dexheimer Carneiro
 
     TODO: Correct import/export errors
  */
 
-import Ball from "Ball";
-
 function loop(){
 
-    var ball1 = new Ball(5, 5, 1, 1, 2, "red");
+    var ball1 = new Ball(1, 1, 10, 10, 5, "red");
 
+    ball1.update();
     ball1.draw();
-    
+
+    // console.clear();
+    // console.log("Calling loop function");
+
     requestAnimationFrame(loop);
 }
 
-loop(); 
+loop();
