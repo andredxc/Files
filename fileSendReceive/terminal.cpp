4887d63dd8e288405106686a34f0034351f54008
Andre D. Carneiro <andre.dxc@hotmail.com>
2020-03-22 03:32:18 -0300

    Adds terminal file

---
+++ fileSendReceive/terminal.cpp
@@ -0,0 +1,28 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <iostream>
+
+using namespace std;
+
+
+list<string>* getCommand(){
+
+	char fullCommand[256];
+	list<char*> lst_result;
+
+	// Reads entire command from stdin
+	// TODO: Check for segfault here
+	scanf("%s", fullCommand);
+}
+
+
+
+int main(int argc, char** argcv){
+
+
+
+
+
+
+
+}
